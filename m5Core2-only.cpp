09b4aa2a6f707203c3f9c9fce202b01ca79a6ebf
Don Wade <don_wade@rogers.com>
2025-08-29 23:30:05 -0400

    another bat fix

--- m5Core2-only.cpp
+++ m5Core2-only.cpp
@@ -59,13 +59,14 @@ void lclear(void)
 //--------------------------------------------------
 bool getBatteryStats (batt_stats *reply)
 {
-	const float fMAX_BAT_mV = 4200;
+	const float fMAX_BAT_mV = 4200; 
 	const float fMIN_BAT_mV = 3100;
-
+	const float fRange = fMAX_BAT_mV - fMIN_BAT_mV;
+	
 	bool isCharging = M5.Power.isCharging();
 
 	float volt_mV = M5.Power.getBatteryVoltage();
-	int percent = (volt_mV - fMIN_BAT_mV) * 100. / fMAX_BAT_mV;
+	int percent = (volt_mV - fMIN_BAT_mV) * 100. / fRange;
 
 	int current_mA = M5.Power.getBatteryCurrent();
 
